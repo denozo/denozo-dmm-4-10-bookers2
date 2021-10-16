c36e64aa89584272c459371569443dc3ae9d287c
denozo <n.demachi23@gmail.com>
2021-10-16 13:03:56 +0000

    [Add]4-10 サインイン、ログイン画面レイアウト編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   
   get "homes/about" => "homes#about", as: "about"
   
-  resources :users, only: [:index, :show, :edit, :update,:create]
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   
 end
