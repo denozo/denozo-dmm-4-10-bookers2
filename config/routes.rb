c284863679c227ad7c773f3534d565cf977bd526
denozo <n.demachi23@gmail.com>
2021-10-16 01:55:44 +0000

    [Add]4-10 他人のユーザー情報編集画面に遷移させない

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  get "about" => "homes#about"
+  get "homes/about" => "homes#about", as: "about"
   
   resources :users, only: [:index, :show, :edit, :update,:create]
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
