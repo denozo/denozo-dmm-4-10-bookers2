4d4e59f7fb0294ab32e5d58c2b40f726e06a8ce8
denozo <n.demachi23@gmail.com>
2021-10-17 07:24:20 +0000

    [Fix]ヘッダーリンク設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  get 'homes/about', to: 'homes#about'
+  get 'home/about', to: 'homes#about'
   
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
